8e4865c697b84a5ec10f0a9df9f66ceafd4a4bd8
Luis Razo <lrazo@hotmail.com>
2016-04-21 01:36:49 +0200

    First search approach

--- program.py
+++ program.py
@@ -1,3 +1,6 @@
+import os
+
+
 def main():
     print_header()
     folder = get_folder_from_user()
@@ -11,7 +14,10 @@ def main():
         print("Wa can't search for nothing!")
         return
 
-    search_folders(folder, text)
+    matches = search_folders(folder, text)
+
+    for m in matches:
+        print(m)
 
 
 def print_header():
@@ -21,15 +27,49 @@ def print_header():
 
 
 def get_folder_from_user():
-    pass
+    folder = input('What folder do you want to search? ')
+    if not folder or not folder.strip():
+        return None
+    if not os.path.isdir(folder):
+        return None
+    return os.path.abspath(folder)
+
 
 
 def get_search_text_from_user():
-    pass
-
+    text = input('What are you searching for [single phrases only]? ')
+    return text.lower()
 
 def search_folders(folder, text):
-    pass
+    print("Would search {} for {}".format(folder, text))
+
+    all_matches =[]
+
+    items = os.listdir(folder)
+
+    for item in items:
+        full_item = os.path.join(folder, item)
+        if os.path.isdir(full_item):
+            continue
+
+        matches = search_file(full_item, text)
+
+        all_matches.extend(matches)
+
+    return all_matches
+
+def search_file(filename, search_text):
+
+    matches = []
+
+    with open(filename, 'r', encoding='utf-8') as fin:
+
+        for line in fin:
+            if line.lower().find(search_text) >= 0:
+                matches.append(line)
+
+    return matches
+
 
 
 if __name__ == '__main__':
